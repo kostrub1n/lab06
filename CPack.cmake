ece6a0f7d233d66048d3a2db4b5626bb3cd09cb4
kostrub1n <144800787+kostrub1n@users.noreply.github.com>
2024-05-28 10:19:01 +0300

    Update CPack.cmake

--- CPack.cmake
+++ CPack.cmake
@@ -2,8 +2,6 @@ include(InstallRequiredSystemLibraries)
 
 set(CPACK_PACKAGE_VERSION ${PRINT_VERSION})
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "C++ app for solving equations")
-set(CPACK_RESOURCE_FILE_LICENSE ${CMAKE_CURRENT_SOURCE_DIR}/LICENSE)
-set(CPACK_RESOURCE_FILE_README ${CMAKE_CURRENT_SOURCE_DIR}/README.md)
 
 set(CPACK_SOURCE_IGNORE_FILES 
 "\\\\.cmake;/.build/;/.git/;/.github/"
